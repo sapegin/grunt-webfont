5d6098d16d2b7d9ec8a1e7309e9d0d8bed41aa56
Tyler Beck <tyler@tylerbeck.com>
2013-11-06 15:20:03 -0500

    demo html font path fix

--- tasks/webfont.js
+++ tasks/webfont.js
@@ -291,7 +291,7 @@ module.exports = function(grunt) {
 			// HTML should not contain relative paths
 			// If some styles was not included in CSS we should include them in HTML to properly render icons
 			var relativeRe = new RegExp(o.relativeFontPath, 'g');
-			var htmlRelativeFontPath = appendSlash(path.relative(o.destCss, o.dest));
+			var htmlRelativeFontPath = appendSlash(path.relative(o.destHtml, o.dest));
 			var context = _.extend(o, {
 				fontSrc1: o.fontSrc1.replace(relativeRe, htmlRelativeFontPath),
 				fontSrc2: o.fontSrc2.replace(relativeRe, htmlRelativeFontPath),
