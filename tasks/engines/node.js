256273ca2f9e112d68792967f72f82846aba4527
Artem Sapegin <artem@sapegin.ru>
2013-11-27 17:31:20 +0400

    Node engine: generate SVG font in memory, generate only needed font types.

--- tasks/engines/node.js
+++ tasks/engines/node.js
@@ -13,6 +13,7 @@ module.exports = function(grunt, o, done) {
 	var path = require('path');
 	var async = require('async');
 	var _ = require('lodash');
+	var StringDecoder = require('string_decoder').StringDecoder;
 	var svgicons2svgfont = require('svgicons2svgfont');
 	var svg2ttf = require('svg2ttf');
 	var ttf2woff = require('ttf2woff');
@@ -20,80 +21,110 @@ module.exports = function(grunt, o, done) {
 
 	// @todo Append hashes
 	// @todo Autohint TTF
-	// @todo Generate only needed font types
-	// @todo Parallelize WOFF and EOT generation
 	// @todo Fix line height, kerning, sizes, etc. (see generate.py)
 	// @todo SVGO for SVG font and for every glyph (?)
 	// @todo Tweak SVG (as in FF script)
-	// @todo Make everything works in memory (don’t write SVG font to file and read it back)
 	// @todo Ligatures
-
-	var svgFontPath = fontPath('svg');
-	var ttfFont;
 
 	var result = {
 		fontName: o.fontName,
 		glyphs: [],
 		codepoints: []
 	};
+	var fonts = {};
 
+	var generators = {
+		svg: function(allDone) {
+			var steps = 2;
+			var done = function() {
+				steps--;
+				if (steps === 0) allDone(font);
+			};
+
+			var font = '';
+			var decoder = new StringDecoder('utf8');
+			var stream = svgicons2svgfont(o.files, {
+				fontName: o.fontName,
+				callback: function(glyphs) {
+					result.glyphs = _.pluck(glyphs, 'name');
+					result.codepoints = _.pluck(glyphs, 'codepoint');
+					done();
+				}
+			});
+			stream.on('data', function(chunk) {
+				font += decoder.write(chunk);
+			});
+			stream.on('end', function() {
+				fonts.svg = font;
+				done();
+			});
+		},
+
+		ttf: function(done) {
+			getFont('svg', function(svgFont) {
+				var font = svg2ttf(svgFont, {});
+				font = new Buffer(font.buffer);
+				fonts.ttf = font;
+				done(font);
+			});
+		},
+
+		woff: function(done) {
+			getFont('ttf', function(ttfFont) {
+				var font = ttf2woff(new Uint8Array(ttfFont), {});
+				font = new Buffer(font.buffer);
+				fonts.woff = font;
+				done(font);
+			});
+		},
+
+		eot: function(done) {
+			getFont('ttf', function(ttfFont) {
+				var font = ttf2eot(new Uint8Array(ttfFont));
+				font = new Buffer(font.buffer);
+				fonts.eot = font;
+				done(font);
+			});
+		}
+	};
+
+
+	var steps = [];
+
+	// Font types
+	o.types.forEach(function(type) {
+		steps.push(createFontWriter(type));
+	});
+
+	steps.push(allDone);
 
 	// Run!
-	async.waterfall([
-		svgFilesToSvgFont,
-		svgFontToTtfFont,
-		ttfFontToWoffFont,
-		ttfFontToEotFont,
-		allDone
-	]);
+	async.waterfall(steps);
 
+	function getFont(type, done) {
+		if (fonts[type]) {
+			done(fonts[type]);
+		}
+		else {
+			generators[type](done);
+		}
+	}
 
-	// Convert SVG files to SVG font
-	function svgFilesToSvgFont(done) {
-		svgicons2svgfont(o.files, svgFontPath, {
-			fontName: o.fontName,
-			callback: function(glyphs) {
-				result.glyphs = _.pluck(glyphs, 'name');
-				result.codepoints = _.pluck(glyphs, 'codepoint');
+	function createFontWriter(type) {
+		return function(done) {
+			getFont(type, function(font) {
+				fs.writeFileSync(getFontPath(type), font);
 				done();
-			}
-		});
+			});
+		};
 	}
 
-	// Create TTF font
-	function svgFontToTtfFont(done) {
-		ttfFont = svg2ttf(fs.readFileSync(svgFontPath, {encoding: 'utf-8'}), {});
-		ttfFont = new Buffer(ttfFont.buffer);
-		fs.writeFileSync(fontPath('ttf'), ttfFont);
-		done();
-	}
-
-	// Create WOFF font
-	function ttfFontToWoffFont(done) {
-		var woff = ttf2woff(new Uint8Array(ttfFont), {});
-		woff = new Buffer(woff.buffer);
-		fs.writeFileSync(fontPath('woff'), woff);
-		done();
-	}
-
-	// Create EOT font
-	function ttfFontToEotFont(done) {
-		var eot = ttf2eot(new Uint8Array(ttfFont));
-		eot = new Buffer(eot.buffer);
-		fs.writeFileSync(fontPath('eot'), eot);
-		done();
+	function getFontPath(type) {
+		return path.join(o.dest, o.fontName + '.' + type);
 	}
 
 	function allDone() {
 		done(result);
 	}
 
-
-	//
-	// Helpers
-	//
-
-	function fontPath(type) {
-		return path.join(o.dest, o.fontName + '.' + type);
-	}
 };
