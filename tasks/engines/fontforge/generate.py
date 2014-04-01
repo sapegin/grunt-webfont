9997ee3be1af6f8d5915d5fb327daf361072f099
Artem Sapegin <artem@sapegin.ru>
2014-04-01 10:34:01 +0400

    Tweak strings (should also fix #130).

--- tasks/engines/fontforge/generate.py
+++ tasks/engines/fontforge/generate.py
@@ -99,7 +99,7 @@ generate(fontfile + '.ttf')
 # Hint the TTF file
 # ttfautohint is optional
 if find_executable('ttfautohint'):
-	call("ttfautohint --symbol --fallback-script=latn --windows-compatibility --no-info '" + fontfile + ".ttf' '" + fontfile + "-hinted.ttf' && mv '" + fontfile + "-hinted.ttf' '" + fontfile + ".ttf'", shell=True)
+	call('ttfautohint --symbol --fallback-script=latn --windows-compatibility --no-info "%(font)s.ttf" "%(font)s-hinted.ttf" && mv "%(font)s-hinted.ttf" "%(font)s.ttf"' % {'font': fontfile}, shell=True)
 	f = fontforge.open(fontfile + '.ttf')
 
 # SVG
@@ -122,7 +122,7 @@ if 'woff' in args['types']:
 # EOT
 if 'eot' in args['types']:
 	# eotlitetool.py script to generate IE7-compatible .eot fonts
-	call("python '" + scriptPath + "/../../bin/eotlitetool.py' '" + fontfile + ".ttf' --output '" + fontfile + ".eot'", shell=True)
+	call('python "%(path)s/../../bin/eotlitetool.py" "%(font)s.ttf" --output "%(font)s.eot"' % {'path': scriptPath, 'font': fontfile}, shell=True)
 
 # Delete TTF if not needed
 if not 'ttf' in args['types']:
